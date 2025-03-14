0a6b1403352d1a86b7978e48fee9a9d792d953ce
Tiberius202 <tiberius202@gmail.com>
2025-03-14 11:49:26 -0400

    Updated fish config with conda

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,17 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 
 # opam configuration
 source /home/jonathan/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/jonathan/miniconda3/bin/conda
+    eval /home/jonathan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/jonathan/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/jonathan/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/jonathan/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
